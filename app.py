d3cf19a9f16966e94566d6f49860bb531bddc4aa
Леонид Каретников <poczta130@gmail.com>
2022-11-18 12:00:26 +0000

    req

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pickle
 import requests
 import json
 
-# model1 = dill.load(open('model1.pkl', 'rb')) #чтение модели
-# model2 = dill.load(open('model2.pkl', 'rb')) #чтение модели
-
 model1 = pickle.load(open('model1.pkl', 'rb')) #чтение модели
 model2 = pickle.load(open('model2.pkl', 'rb')) #чтение модели
+
+# model1 = pickle.load('model1.pkl', 'rb') #чтение модели
+# model2 = pickle.load('model2.pkl', 'rb') #чтение модели
 path = "data/ebw_data.csv"
 df = pd.read_csv(path, sep = ",", encoding = "utf-8") #чтение данных
 
@@ -33,10 +33,10 @@ FP = st.number_input('FP', min_value=np.int64(df['FP'].min()), max_value=np.int6
 data = {'IW': IW, 'IF': IF, 'VW': VW, 'FP': FP} #создание словаря с данными
 
 prediction1 = model1.predict(pd.DataFrame(data, index=[0])) #предсказание
-# prediction2 = model2.predict(pd.DataFrame(data, index=[0]))
+prediction2 = model2.predict(pd.DataFrame(data, index=[0]))
 
-#prediction button
+# prediction button
 # if st.button('Предсказать'):
-# st.write('прогнозирование глубины(depth): ', round(prediction1[0],2))
-# st.write('прогнозирование ширины(width): ', round(prediction2[0],2))
+st.write('прогнозирование глубины(depth): ', round(prediction1[0],2))
+st.write('прогнозирование ширины(width): ', round(prediction2[0],2))
 
